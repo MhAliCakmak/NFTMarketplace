5559c7b5ecd0db8e925a63644a1c73830cbc8cc1
mehmet5643 <202803055@ogr.cbu.edu.tr>
2023-10-29 15:18:06 +0300

    tailwind updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   purge: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './Components/**/*.{js,ts,jsx,tsx}',
   ],
+
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
