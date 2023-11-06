c92260476e66cd7fa01c20deffbfd83414b940ef
mehmet5643 <202803055@ogr.cbu.edu.tr>
2023-11-06 09:54:41 +0300

    Add metadata and favicon to _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>NFT Marketplace | Buy and Sell Non-Fungible Tokens</title>
+        <meta
+          name="description"
+          content="NFT Marketplace to buy and sell Non-Fungible Tokens"
+        />
+        <meta
+          property="og:title"
+          content="NFT Marketplace | Buy and Sell Non-Fungible Tokens"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
